a390e252453bfe2f32e68200a93cf87dd4a6bbaa
Sarah Rose Hassan <sarahrosehassan@users.noreply.github.com>
2026-06-04 16:33:19 -0400

    Fix competency name capitalization in seed data

--- seed.sql
+++ seed.sql
@@ -4,7 +4,7 @@ INSERT INTO students VALUES('Sarah', 'Hassan', '500880917');
 
 -- competencies
 INSERT INTO competencies (name, description) VALUES ('Nested loops', 'a programming structure where one loop is placed entirely inside the body of another loop');
-INSERT INTO competencies (name, description) VALUES ('recursion', 'occurs when a function or process calls itself to solve a smaller, self-similar piece of a larger problem');
+INSERT INTO competencies (name, description) VALUES ('Recursion', 'occurs when a function or process calls itself to solve a smaller, self-similar piece of a larger problem');
 INSERT INTO competencies (name, description) VALUES ('Pointers', 'a special programming variable that stores the memory address of another piece of data');
 
 -- achievements (student_number, competency_id, date_achieved)
